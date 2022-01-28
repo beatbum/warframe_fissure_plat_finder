bd018565db2af3acca94c32f40d660316cf8a1d7
Jake Klovenski <j.beatbum@gmail.com>
2022-01-28 14:38:37 -0600

    add support for gui

---
+++ gui.py
@@ -0,0 +1,47 @@
+import tkinter as tk
+import screenshot_and_parse as sp
+
+
+class Gui(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self["borderwidth"] = 10
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.create_input_button()
+        self.create_output_frame()
+
+    def create_output_frame(self):
+        self.output_frame = tk.Frame(self, padx=10, pady=10, borderwidth=2, relief="sunken")
+        self.output = [*range(4)]
+        print(self.output)
+        for y in range(4):
+            self.output[y] = tk.Label(self.output_frame, text="Output will go here")
+            self.output[y].grid(row=0, column=y)
+            self.output_frame.grid(row=0, column=0)
+        print(self.output)
+
+    def create_input_button(self):
+        self.scan = tk.Button(self)
+        self.scan["text"] = "Scan screen for items"
+        self.scan["command"] = self.get_data
+        self.scan.grid(row=1, column=0)
+
+    def get_data(self):
+        place_in_list = 0
+        prices = sp.main_logic()
+        for item in prices.keys():
+            result = prices[item].result()
+            if result is not None:
+                print("{item}: Price {price}".format(item=item, price=result))
+                self.output[place_in_list]["text"] = "{item}\n{price}P".format(item=item, price=result)
+                place_in_list += 1
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = Gui(master=root)
+    app.mainloop()
--- screenshot_and_parse.py
+++ screenshot_and_parse.py
@@ -6,60 +6,72 @@ import requests
 from concurrent import futures
 from pynput.keyboard import Listener
 from pynput import keyboard
+import logging
+
 
 URL = "https://api.warframe.market/v1"
 
 
 def preprocess_frame(frame):
+    logging.debug("Preprocessing screenshot")
     frame = get_grayscale(frame)
     return frame
 
 
 def canny(frame):
+    logging.debug("Applying Canny filter to screenshot")
     frame = cv2.Canny(frame, 100, 200)
     return frame
 
 
 def opening(frame):
+    logging.debug("Applying opening effect to screenshot")
     kernel = np.ones((5, 5), np.uint8)
     frame = cv2.morphologyEx(frame, cv2.MORPH_OPEN, kernel)
     return frame
 
 
 def erosion(frame):
+    logging.debug("Applying erosion effect to screenshot")
     kernel = np.ones((5, 5), np.uint8)
     frame = cv2.erode(frame, kernel, iterations=1)
     return frame
 
 
 def dilate(frame):
+    logging.debug("Applying dilation to screenshot")
     kernel = np.ones((5, 5), np.uint8)
     frame = cv2.dilate(frame, kernel, iterations=1)
     return frame
 
 
 def thresholding(frame):
+    logging.debug("Applying thresholding to screenshot")
     frame = cv2.threshold(frame, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
     return frame
 
 
 def remove_noise(frame):
+    logging.debug("Removing noise from screenshot")
     frame = cv2.medianBlur(frame, 5)
     return frame
 
 
 def get_grayscale(frame):
+    logging.debug("Converting screenshot to grayscale")
     frame = cv2.cvtColor(np.array(frame), cv2.COLOR_RGB2GRAY)
     return frame
 
 
 def run_easyocr(frame):
+    logging.debug("Executing ocr on screenshot")
     reader = easyocr.Reader(['en'])
     output = reader.readtext(frame)
     return output
 
 
 def get_price(item):
+    logging.debug("Getting the price of {item}".format(item=item))
     market = query_market(item)
     if market.status_code == 200:
         market = market.json()['payload']['orders']
@@ -73,8 +85,11 @@ def query_market(item):
     item = str.lower(item)
     item = str.replace(item, ' ', '_')
     get_text = '/items/{item}/orders'.format(item=item)
+    full_url = URL + get_text
     headers = {'accept': 'application/json', 'Platform': 'pc'}
-    market = requests.get(url=URL + get_text, headers=headers)
+    logging.debug("Sending request to {url} along with {headers}".format(url=full_url, headers=headers))
+    market = requests.get(url=full_url, headers=headers)
+    logging.debug("Received {data}".format(data=market.status_code))
     return market
 
 
@@ -101,7 +116,7 @@ def print_average_price(item, order_price_and_quantities):
         sum_plat += price * quantity
         sum_quantity += quantity
     average_price = sum_plat / sum_quantity
-    print("{item}: Average Price: {price}".format(item=item, price=average_price))
+    logging.debug("{item}: Average Price: {price}".format(item=item, price=average_price))
     return average_price
 
 
@@ -109,26 +124,36 @@ def main(key):
     try:
         if key == keyboard.Key.page_down:
             print("entering main loop")
-            executor = futures.ThreadPoolExecutor(max_workers=6)
-            frame = pyautogui.screenshot()
-            frame = np.array(frame)
-            #frame = cv2.imread("fissure_reward_2.jpg")
-            x = len(frame[0])
-            y = len(frame)
-            frame = frame[y//10:5*y//10, 0:x]
-            frame = preprocess_frame(frame)
-            words = run_easyocr(frame)
-            prices = dict()
-            for word in words:
-                location, item, _ = word
-                cv2.rectangle(frame, location[0], location[2], (255, 0, 0), 1)
-                prices[item] = executor.submit(get_price, item)
-            futures.wait(prices.values())
-            #cv2.imshow('frame', frame)
-            #cv2.waitKey(0)
-            #cv2.destroyAllWindows()
+            prices = main_logic()
+            for item in prices:
+                price = prices[item].result()
+                if price is not None:
+                    print("{item}: {price}".format(item=item, price=price))
     except AttributeError:
         print(key)
+
+
+def main_logic():
+    print("entering main_logic")
+    executor = futures.ThreadPoolExecutor(max_workers=8)
+    frame = pyautogui.screenshot()
+    frame = np.array(frame)
+    #frame = cv2.imread("fissure_reward_2.jpg")
+    x = len(frame[0])
+    y = len(frame)
+    frame = frame[y // 10:5 * y // 10, 0:x]
+    frame = preprocess_frame(frame)
+    words = run_easyocr(frame)
+    prices = dict()
+    for word in words:
+        location, item, _ = word
+        cv2.rectangle(frame, location[0], location[2], (255, 0, 0), 1)
+        prices[item] = executor.submit(get_price, item)
+    futures.wait(prices.values())
+    # cv2.imshow('frame', frame)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    return prices
 
 
 if __name__ == '__main__':
